f2fbb06a26a4fcb5176579fda656c6a60c0d41f5
ferdlg <ferdlgpar@gmail.com>
2024-04-09 23:07:53 -0500

    settings

--- jjsystemPrueba/jjsystemPrueba/settings.py
+++ jjsystemPrueba/jjsystemPrueba/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import warnings
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
