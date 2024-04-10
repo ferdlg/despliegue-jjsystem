a3ebfcb6b6baa7e568ba7a20b0c9c3d2e5aad965
ferdlg <ferdlgpar@gmail.com>
2024-04-09 23:02:12 -0500

    ...

--- jjsystemPrueba/jjsystemPrueba/settings.py
+++ jjsystemPrueba/jjsystemPrueba/settings.py
@@ -206,6 +206,3 @@ MEDIA_URL = 'images/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 # Suprimir todas las advertencias relacionadas con la asignación de números a instancias
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME :
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
