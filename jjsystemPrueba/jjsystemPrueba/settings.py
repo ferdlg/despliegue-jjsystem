0e584d7d1291c23a4d846bff0b7a86cec6721ab5
ferdlg <ferdlgpar@gmail.com>
2024-04-12 10:28:10 -0500

    ...

--- jjsystemPrueba/jjsystemPrueba/settings.py
+++ jjsystemPrueba/jjsystemPrueba/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['jjsystem.onrender.com','127.0.0.1','localhost']
-CSRF_TRUSTED_ORIGINS = ['jjsystem.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://jjsystem.onrender.com/']
 
 
 
