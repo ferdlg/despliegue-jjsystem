e9460486c81daa335140d77243919426accbda3b
ferdlg <ferdlgpar@gmail.com>
2024-04-11 21:53:59 -0500

    ...

--- jjsystemPrueba/jjsystemPrueba/settings.py
+++ jjsystemPrueba/jjsystemPrueba/settings.py
@@ -183,7 +183,7 @@ STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
