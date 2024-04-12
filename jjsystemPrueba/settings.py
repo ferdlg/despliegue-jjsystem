8c1a2145a32d966ea4df60289b5a88f6ba16e175
ferdlg <ferdlgpar@gmail.com>
2024-04-12 14:24:46 -0500

    Revert "intento de despliegue con railway"
    This reverts commit da38b5b784932a755c612c54cfb6693c8d265d2e.

--- jjsystemPrueba/settings.py
+++ jjsystemPrueba/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'RENDER' not in os.environ
-DEBUG = False
-ALLOWED_HOSTS = ['jjsystem-production.up.railway.app/','jjsystem.onrender.com','127.0.0.1','localhost']
-CSRF_TRUSTED_ORIGINS = ['https://jjsystem-production.up.railway.app/']
+DEBUG = 'RENDER' not in os.environ
+
+ALLOWED_HOSTS = ['jjsystem.onrender.com','127.0.0.1','localhost']
+CSRF_TRUSTED_ORIGINS = ['https://jjsystem.onrender.com/']
 
 
 
@@ -167,8 +167,11 @@ EMAIL_HOST_PASSWORD = 'oybttoqvhrehynlq'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ServicioTecnico', 'static'),
