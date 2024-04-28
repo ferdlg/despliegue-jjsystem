5dd0eb174955e5fcde245f86ee7cc683254e273d
ferdlg <ferdlgpar@gmail.com>
2024-04-28 09:34:03 -0500

    urls

--- jjsystemPrueba/jjsystemPrueba/urls.py
+++ jjsystemPrueba/jjsystemPrueba/urls.py
@@ -7,14 +7,14 @@ from ProductosServicios.views import landing, servicios, productos
 
 
 urlpatterns = [
-    path('', landing, name = 'landing'),
+    path('', landing, name= 'landing'),
     path('admin/', admin.site.urls),
     path('account/', include('Account.urls')),
     path('productos_servicios/', include('ProductosServicios.urls')),
     path('servicio_tecnico/', include('ServicioTecnico.urls')),
     path('envios/', include('Envios.urls')),
     path('pqrsf/', include('Pqrsf.urls')),
-    path('servicios/', servicios, name ='servicios'),
+    path('servicios/', servicios, name='servicios'),
     path('productos/', productos, name='productos')
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
